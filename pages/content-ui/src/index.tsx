8b247c73e138005f28fb83d67eb4c3dff790d5f6
ba144220 <hs41511@gmail.com>
2025-05-02 17:25:23 -0700

    test: make primary button blue

--- pages/content-ui/src/index.tsx
+++ pages/content-ui/src/index.tsx
@@ -32,3 +32,40 @@ if (navigator.userAgent.includes('Firefox')) {
 
 shadowRoot.appendChild(rootIntoShadow);
 createRoot(rootIntoShadow).render(<App />);
+
+console.log('content-ui script loaded');
+// get the current url
+const url = window.location.href;
+console.log('current url', url);
+
+// delay function
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getSplitPrimaryButton(
+  maxRetries: number = 10,
+  interval: number = 1000, // 1 second
+): Promise<HTMLElement | null> {
+  let retries = 0;
+  while (retries < maxRetries) {
+    const splitPrimaryButton = document.querySelector('.splitPrimaryButton');
+    if (splitPrimaryButton) {
+      return splitPrimaryButton as HTMLElement;
+    }
+    console.log('waiting for splitPrimaryButton', retries);
+    retries++;
+    await delay(interval);
+  }
+  return null;
+}
+(async () => {
+  // delay 2 seconds
+  await delay(5000);
+  const splitPrimaryButton = await getSplitPrimaryButton();
+  console.log('splitPrimaryButton', splitPrimaryButton);
+  if (splitPrimaryButton) {
+    splitPrimaryButton.style.border = '4px solid blue';
+    // splitPrimaryButton.classList.add('border-2', 'border-teal-500');
+  }
+})();
